d375e553021aed7f126400c9157751c8b6125bac
Manav Jain <118716368+ManavJain01@users.noreply.github.com>
2024-03-05 02:58:48 +0530

    commit

---
+++ MainFile/objects.js
@@ -0,0 +1,17 @@
+const object = [
+  {
+    link:"https://github.com/ManavJain01/MachineLearning/tree/main/my-projects/Naive%20Bayes/The%20Last%20of%20us",
+    h1:"Project 1",
+    img:"../my-projects/Sentiment Analysis/Images/the last of us.png",
+    p1:"Sentiment Analysis/Naive Bayes",
+    p2:"The Last of Us"
+  },
+  {
+    link:"#",
+    h1:"Project 2",
+    img:"../my-projects/XGB Algorithm/CICIDS 2018/Images/img1.png",
+    p1:"Algorithm :: XGB",
+    p2:"Dataset :: CICIDS 1018",
+    p3:"Searches :: Grid & Random Search"
+  }
+]
